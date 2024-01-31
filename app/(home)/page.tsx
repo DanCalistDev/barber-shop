176d6c2838bc63df133294c6483d9954ebc6bc4f
Dancalist <dancalist.dev@gmail.com>
2024-01-30 22:33:32 -0300

    feat: :sparkles: add initial search component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,11 @@
 import { format } from "date-fns";
 import Header from "../_components/header";
 import { ptBR } from "date-fns/locale";
+import { Search } from "./_components/search";
 
 export default function Home() {
   return (
-    <div>
+    <div className="">
       <Header />
       <div className="px-5 pt-5">
         <h2 className="text-xl font-bold">Olá, Daniel!</h2>
@@ -13,6 +14,9 @@ export default function Home() {
             locale: ptBR,
           })}
         </p>
+      </div>
+      <div className="px-5 mt-6">
+      <Search />
       </div>
     </div>
   );
