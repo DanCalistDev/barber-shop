85e136415ecb89a12551a31dc2533f34a518341d
Dancalist <dancalist.dev@gmail.com>
2024-02-06 22:23:52 -0300

    feat: :sparkles: ensure unauthenticated use is redirected to login on booking click

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,6 +1,8 @@
 import { db } from "@/app/_lib/prisma";
 import { BarbershopInfo } from "./_components/barbershop-info";
 import { ServiceItem } from "./_components/service-item";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/_lib/auth";
 
 interface BarbershopDetailsPageProps {
   params: {
@@ -11,6 +13,9 @@ interface BarbershopDetailsPageProps {
 const BarbershopDetailsPage = async ({
   params,
 }: BarbershopDetailsPageProps) => {
+
+  const session = await getServerSession(authOptions);
+
   if (!params.id) {
     //TODO: redirecionar para home page
     return null;
@@ -35,7 +40,7 @@ const BarbershopDetailsPage = async ({
       <BarbershopInfo barbershop={barbershop} />
       <div className="px-5 flex flex-col gap-4 py-6">
         {barbershop.services.map((service) => (
-          <ServiceItem key={service.id} service={service} />
+          <ServiceItem key={service.id} service={service} isAuthenticated={!!session?.user} />
         ))}
       </div>
     </div>
