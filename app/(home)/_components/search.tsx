9baea2c44f56475dc1bc1399eddc4f27f0933a3d
Dancalist <dancalist.dev@gmail.com>
2024-02-12 00:31:09 -0300

    feat: :sparkles: ensure user can search for barbershops

--- app/(home)/_components/search.tsx
+++ app/(home)/_components/search.tsx
@@ -3,17 +3,61 @@
 import { Button } from "@/app/_components/ui/button";
 import { Input } from "@/app/_components/ui/input";
 import { SearchIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { Form, FormControl, FormField, FormItem, FormMessage } from "@/app/_components/ui/form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-export const Search = () => {
-    return ( 
-          
-        <div className="flex items-center gap-2">
-           <Input placeholder="Busque por uma barbearia..."/>
-           <Button variant="default">
-              <SearchIcon size={20}/>
-           </Button>
 
-        </div>
-     );
+const formSchema = z.object({
+  search: z
+    .string({
+      required_error: "Campo obrigatório.",
+    })
+    .trim()
+    .min(1, "Campo obrigatório."),
+});
+
+interface SearchProps {
+  defaultValues?: z.infer<typeof formSchema>;
 }
- 
+
+export const Search = ({ defaultValues }: SearchProps) => {
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues,
+  });
+
+  const handleSubmit = (data: z.infer<typeof formSchema>) => {
+    router.push(`/barbershops?search=${data.search}`);
+  };
+
+  return (
+    <div className="flex items-center gap-2">
+      <Form {...form}>
+        <form className="flex w-full gap-4" onSubmit={form.handleSubmit(handleSubmit)}>
+          <FormField
+            control={form.control}
+            name="search"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Input placeholder="Busque por uma barbearia..." {...field} />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button variant="default" type="submit">
+            <SearchIcon size={20} />
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
+};
